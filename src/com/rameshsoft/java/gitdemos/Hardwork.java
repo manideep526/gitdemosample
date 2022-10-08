22626b721907737afcad22df2fe294ae9e6a0df1
manideep526 <sajjamanideep526@gmail.com>
2022-10-08 11:21:13 +0530

    I added some modifications

--- src/com/rameshsoft/java/gitdemos/Hardwork.java
+++ src/com/rameshsoft/java/gitdemos/Hardwork.java
@@ -3,11 +3,11 @@ package com.rameshsoft.java.gitdemos;
 public class Hardwork {
 	public static void main(String[] args) {
 		System.out.println("9177791456");
-<<<<<<< HEAD
                 System.out.println("JOB");
-=======
-		System.out.println("HELLO");
->>>>>>> 4439cea68568fcc6036bae6c03b5f97b9ae76021
+                System.out.println("HELLO");
+
+		
+
 	}
 
 }
