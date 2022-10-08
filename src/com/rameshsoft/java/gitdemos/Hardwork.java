e03ac91cfd9494018c99287061cda3481ff694f6
manideep526 <sajjamanideep526@gmail.com>
2022-10-08 10:54:37 +0530

    added new files by dev1

---
+++ src/com/rameshsoft/java/gitdemos/Hardwork.java
@@ -0,0 +1,8 @@
+package com.rameshsoft.java.gitdemos;
+
+public class Hardwork {
+	public static void main(String[] args) {
+		System.out.println("9177791456");
+	}
+
+}
--- src/com/rameshsoft/java/gitdemos/Job.java
+++ src/com/rameshsoft/java/gitdemos/Job.java
@@ -4,6 +4,7 @@ public class Job {
 	public static void main(String[] args) {
 		
 		System.out.println("JOB");
+		System.out.println("JOB = HARDWORK");
 	}
 
 }
